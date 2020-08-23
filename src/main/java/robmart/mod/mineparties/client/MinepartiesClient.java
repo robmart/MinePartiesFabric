5c2cceac8c34082a694d7084b073f04149687fa9
Robin <robinmartensson01@gmail.com>
2020-08-23 12:36:22 +0200

    Basic mod

---
+++ src/main/java/robmart/mod/mineparties/client/MinepartiesClient.java
@@ -0,0 +1,13 @@
+package robmart.mod.mineparties.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MinepartiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/robmart/mod/mineparties/common/Mineparties.java
@@ -0,0 +1,10 @@
+package robmart.mod.mineparties.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mineparties implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
