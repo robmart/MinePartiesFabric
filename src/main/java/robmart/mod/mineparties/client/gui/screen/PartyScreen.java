3c710269c09ddef62edcf680aa199174b84398b6
Robmart <robinmartensson01@gmail.com>
2021-09-04 09:19:24 +0200

    Minor addition

--- src/main/java/robmart/mod/mineparties/client/gui/screen/PartyScreen.java
+++ src/main/java/robmart/mod/mineparties/client/gui/screen/PartyScreen.java
@@ -2,28 +2,59 @@ package robmart.mod.mineparties.client.gui.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
+import robmart.mod.mineparties.api.faction.FactionParty;
+import robmart.mod.targetingapifabric.api.Targeting;
+import robmart.mod.targetingapifabric.api.faction.Faction;
 
 public class PartyScreen extends Screen {
     private static final Identifier TEXTURE = new Identifier("mineparties:textures/gui/party.png");
     private static final int TEXTURE_HEIGHT = 166;
     private static final int TEXTURE_WIDTH = 176;
 
+    private TextFieldWidget partyNameWidget;
+    private FactionParty party;
+
     public PartyScreen() {
         super(Text.of("Party"));
     }
 
+    public void loadParty() {
+        for (Faction faction : Targeting.getFactionsFromEntity(client.player)) {
+            if (faction instanceof FactionParty) {
+                party = (FactionParty) faction;
+            }
+        }
+    }
+
     @Override
     protected void init() {
+        loadParty();
 
+        partyNameWidget = new TextFieldWidget(this.textRenderer, this.width / 2 - 80, this.height / 2 - 75,
+                TEXTURE_WIDTH / 2 - 10, 15, new TranslatableText("mineparties.name"));
+        addSelectableChild(partyNameWidget);
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
+
+        if (party == null) {
+            partyNameWidget.render(matrices, mouseX, mouseY, delta);
+        } else {
+            textRenderer.draw(matrices, party.getName(), this.width / 2 - 80, this.height / 2 - 75, 0);
+        }
+    }
+
+    @Override
+    public void renderBackground(MatrixStack matrices) {
+        super.renderBackground(matrices);
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
